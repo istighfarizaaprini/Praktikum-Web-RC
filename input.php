8604f53ff8444bb933a8a3a8ae2c09885f4f862a
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-11-01 16:10:39 +0700

    Update input.php

--- input.php
+++ input.php
@@ -1,10 +1,31 @@
 <html>
 <head>
   <script type="text/javascript" src="jquery-3.5.1.min.js"></script>
+  <style>
+    legend{
+      font-size:30pt;
+      font-family: 'Courier New';
+      margin: 20px 10px 20px 10px;
+      color:black;
+    }
+    form{
+      background-color: rgba(184, 207, 228, 0.897);
+      color: rgb(119, 40, 29);
+      font-family:'Segoe UI';
+      font-size: 35px;
+      text-align: center;
+      width: 40%;
+      margin: 100pt 400pt 50pt 300pt;
+      padding: 20px;
+    }
+    input{
+      width: 35%;
+    }
+  </style>
 </head>
 <body>
 <form method="post" id="form_mhs" >
-  <h1>~Silahkan input data~</h1>
+  <legend><b>~Silahkan input data~</b></legend>
   <label>NIM </label>
   <input type="number" name="nim"><br>
   <label>Nama </label>
