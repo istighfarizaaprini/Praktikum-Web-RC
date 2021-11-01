becf74edd1bae2dcfa32e97c9d39dafddaba6495
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-11-01 15:37:57 +0700

    Create proses.php

---
+++ proses.php
@@ -0,0 +1,18 @@
+<?php
+include "koneksi.php";
+
+$nim = $_POST["nim"];
+$nama = $_POST["nama"];
+$prodi = $_POST["prodi"];
+$angkatan = $_POST["angkatan"];
+
+$sql = "insert into mahasiswa (nim,nama,prodi,angkatan) values('$nim','$nama','$prodi','$angkatan')";
+$hasil = mysqli_query($kon,$sql);
+
+if ($hasil) {
+echo "<script>alert('data berhasil disimpan');
+document.location.href='tampil.php'</script>\n";
+} else {
+echo "<script>alert('data gagal disimpan')";
+}
+?>
