5f870d98d4e4cccc1d393e2e23c06b3b21e8ccb2
Istighfariza Aprini <istighfariza.119140059@student.itera.ac.id>
2021-11-01 17:21:44 +0700

    Update input.php

--- input.php
+++ input.php
@@ -9,22 +9,20 @@
       color:black;
     }
     form{
-      background-color: rgba(184, 207, 228, 0.897);
+      background-color: rgba(200, 180, 181, 0.897);
       color: rgb(119, 40, 29);
       font-family:'Segoe UI';
-      font-size: 35px;
+      font-size: 25px;
       text-align: center;
       width: 40%;
-      margin: 100pt 400pt 50pt 300pt;
+      margin: 150pt 400pt 50pt 300pt;
       padding: 20px;
-    }
-    input{
-      width: 35%;
+      height:50p%;
     }
   </style>
 </head>
 <body>
-<form method="post" id="form_mhs" >
+<form method="post" action="proses.php" >
   <legend><b>~Silahkan input data~</b></legend>
   <label>NIM </label>
   <input type="number" name="nim"><br>
@@ -40,26 +38,7 @@
   </select><br>
   <label>Angkatan </label>
   <input type="number" name="angkatan"><br>
-  <button id="btn_simpan">Simpan</button>
+  <p> <input type="submit" name="simpan" value="simpan"></p>
 </form>
-<script>
-   $(document).ready(function() {
-     $('bnt_simpan').click(function(){
-       var data = $('form_mhs').serialize();
-       $.ajax({
-         type: 'POST',
-         url: "tambah.php",
-         data: data,
-         cache: false,
-         success: function(data) {
-           $('#tampil_data').load("tampil.php");
-          },
-          error: function(request, error) {
-          console.log(arguments);
-          alert(" Can't do because: " + error);
-          }
-       })
-     })
-</script>
 </body>
 </html>
